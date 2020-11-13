0159b81a97a8814edd83c4b2def363dcdabf9448
shashank <sha2nk2012@gmail.com>
2020-11-13 18:04:47 +0000

    Adding first commit for basic spring boot structure

---
+++ src/main/java/ie/tcd/paintsplat/painsplatapp/PainsplatappApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.paintsplat.painsplatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainsplatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainsplatappApplication.class, args);
+	}
+
+}
